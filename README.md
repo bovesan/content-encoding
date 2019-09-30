de5ff6ddd4433db90a8515b3fccb5aa6f3f4b5e0
Bengt Ove Sannes <dev@bovsan.com>
2019-09-30 13:49:12 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# gzip-encoding-test
+# Content-Encoding test
+Tests a browsers ability to inflate gzip encoded responses.
+Must be deployed to a php server.
+
+https://bovesan.com/misc/content-encoding/
---
+++ gzip/index.php
@@ -0,0 +1,23 @@
+<?php
+if ($_GET['source']){
+	show_source(__FILE__, false);
+	exit();
+}
+$output = '';
+// header('Content-type: application/octet-stream');
+if (array_key_exists('Accept-Encoding', apache_request_headers())){
+	$output .= 'Accept-Encoding: '.apache_request_headers()['Accept-Encoding']."\n";
+	if (in_array('gzip', array_map('trim', explode(',', apache_request_headers()['Accept-Encoding'])))){
+		$output .= 'Client accepts gzip'."\n";
+    	ob_start("ob_gzhandler");
+		$output .= 'This output is compressed.'."\n";
+    	echo $output;
+    	ob_end_flush();
+    	exit();
+	} else {
+		$output .= 'gzip not among accepted encodings'."\n";
+	}
+} else {
+	$output .= 'Accept-Encoding header not present'."\n";
+}
+echo $output;
