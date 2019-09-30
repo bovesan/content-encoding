d6c1d546d83e730fab85d9cc970b343d9fb5caae
Bengt Ove Sannes <dev@bovsan.com>
2019-09-30 15:41:55 +0200

    Added proper resource file

--- resource.php
+++ resource.php
@@ -1,11 +1,13 @@
 <?php
 $output = '';
+$output .= 'Request uri: '.$_SERVER['REQUEST_URI'];
 // header('Content-type: application/octet-stream');
 if (array_key_exists('Accept-Encoding', apache_request_headers())){
 	$output .= 'Accept-Encoding: '.apache_request_headers()['Accept-Encoding']."\n";
 	if (in_array('gzip', array_map('trim', explode(',', apache_request_headers()['Accept-Encoding'])))){
 		$output .= 'Client accepts gzip'."\n";
     	ob_start("ob_gzhandler");
+    	if (file_exists($_SERVER['REQUEST_URI']))
 		$output .= 'This output is compressed.'."\n";
     	echo $output;
     	ob_end_flush();
